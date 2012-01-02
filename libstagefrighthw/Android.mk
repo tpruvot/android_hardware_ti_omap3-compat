6dc49f1a8a17b3c87de66b9cbff605d37b13c29f
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-01-02 13:24:39 +0100

    libstagefrighthw: use ANDROID_BUILD_TOP
    Change-Id: Ic5401d6a27a82f1df7051733cd6c0cea16aff59f

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -7,14 +7,14 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS := $(PV_CFLAGS_MINUS_VISIBILITY)
 
 LOCAL_C_INCLUDES:= \
-        $(TOP)/frameworks/base/include/media/stagefright/openmax
+    $(ANDROID_BUILD_TOP)/frameworks/base/include/media/stagefright/openmax
 
-LOCAL_SHARED_LIBRARIES :=       \
-        libbinder               \
-        libutils                \
-        libcutils               \
-        libui                   \
-        libdl                   \
+LOCAL_SHARED_LIBRARIES := \
+    libbinder \
+    libutils  \
+    libcutils \
+    libui     \
+    libdl     \
 
 LOCAL_MODULE := libstagefrighthw
 
