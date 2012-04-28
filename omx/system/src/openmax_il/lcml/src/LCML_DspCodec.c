b4a6950b2f680529a63b1a6a5a389874f41f9791
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-04-28 23:07:43 +0200

    fix to be able to build with gcc 4.6
    Change-Id: I184a0f0ed37394e476cdcce9dcce899a265ffd20

--- omx/system/src/openmax_il/lcml/src/LCML_DspCodec.c
+++ omx/system/src/openmax_il/lcml/src/LCML_DspCodec.c
@@ -1601,8 +1601,9 @@ OMX_ERRORTYPE FreeResources (LCML_DSP_INTERFACE *hInterface)
 {
     OMX_ERRORTYPE eError = OMX_ErrorNone;
     struct LCML_DSP_INTERFACE *codec;
+    struct OMX_TI_Debug dbg = ((LCML_CODEC_INTERFACE*)hInterface->pCodecinterfacehandle)->dbg;
 
-    OMX_PRINT1 ((struct OMX_TI_Debug)(((LCML_CODEC_INTERFACE *)hInterface->pCodecinterfacehandle)->dbg), "%d :: LCML:: FreeResources\n",__LINE__);
+    OMX_PRINT1(dbg, "%d :: LCML:: FreeResources\n",__LINE__);
     if(hInterface->dspCodec != NULL)
     {
         LCML_FREE(hInterface->dspCodec);
@@ -1614,7 +1615,7 @@ OMX_ERRORTYPE FreeResources (LCML_DSP_INTERFACE *hInterface)
         pthread_mutex_destroy(&codec->m_isStopped_mutex);
         pthread_mutex_lock(&codec->mutex);
 
-        OMX_PRINT1 ((struct OMX_TI_Debug)(((LCML_CODEC_INTERFACE *)hInterface->pCodecinterfacehandle)->dbg), "%d :: LCML:: FreeResources\n",__LINE__);
+        OMX_PRINT1(dbg, "%d :: LCML:: FreeResources\n",__LINE__);
         if(codec->g_aNotificationObjects[0]!= NULL)
         {
             LCML_FREE(codec->g_aNotificationObjects[0]);
@@ -1631,8 +1632,8 @@ OMX_ERRORTYPE FreeResources (LCML_DSP_INTERFACE *hInterface)
                 codec->g_aNotificationObjects[2] = NULL;
             }
  #endif
-            OMX_DBG_CLOSE((struct OMX_TI_Debug )(((LCML_CODEC_INTERFACE*)hInterface->pCodecinterfacehandle)->dbg));
-            //LCML_FREE(((struct LCML_CODEC_INTERFACE*)hInterface->pCodecinterfacehandle));
+            OMX_DBG_CLOSE(dbg);
+
             LCML_FREE(hInterface->pCodecinterfacehandle);
             hInterface->pCodecinterfacehandle = NULL;
         }
